e2761f49802d48f30c267d6870afd76955b123d8
thiagoctro <thiagoctro@outlook.com>
2021-03-30 15:34:12 -0300

    update set slider configurations

--- OrganizeApp/app/src/main/java/com/organizeclone/MainActivity.java
+++ OrganizeApp/app/src/main/java/com/organizeclone/MainActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
